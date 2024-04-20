4ff1e39e0f607fe6a24cc580ae3d58a8fde6a36c
201801421 <77879728+kiran933@users.noreply.github.com>
2024-04-20 22:16:18 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,10 +35,14 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        My dear Kannamma<br />
+        I dont know if anyone has given you a love letter hope this letter is unique<br />
+        I dont know if this is the right time but because of all the stress you have taken today,<br />
+        I want to put a smile on your face by proposing you with a love letter.<br />
+        I LOVE YOU AISHUUUU <br />
+        With lots of love <br />
+        Yours most lovingly <br />
+        Your Kiran 
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
