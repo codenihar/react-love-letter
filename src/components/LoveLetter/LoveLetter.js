d311398030b1d1632b89446e2a2393475919b800
Rishimalya <167606431+Rishimalya@users.noreply.github.com>
2024-04-20 13:43:57 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -38,7 +38,6 @@ const LoveLetter = () => {
         mah dear kundanapu bomma🖤,<br />
         never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
         always yours,<br />
-        @niihaaarrrr
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
