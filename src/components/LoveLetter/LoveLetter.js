45a7212d28c869ca2ac99eeb11469c5501ab7093
nareshenukunti <168506248+nareshenukunti@users.noreply.github.com>
2024-04-30 09:26:45 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -37,8 +37,8 @@ const LoveLetter = () => {
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
         mah dear kundanapu bomma🖤,<br />
         never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        always yours kitty,<br />
+        @naresh
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
