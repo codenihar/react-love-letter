d68f5a1ceb950d4869d48a92b64fc16c8a81a27e
veerabhadrasai <veeraallamsetti@gmail.com>
2024-05-10 02:52:21 +0530

    goo

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react';
 import './LoveLetter.css';
 import audioFile from './kushi.mp3';
 
+
 const LoveLetter = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isFullSize, setIsFullSize] = useState(false);
@@ -31,16 +32,32 @@ const LoveLetter = () => {
   };
 
   return (
+    <div className='container'>
     <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        mah dear kundanapu bomma🖤,<br /><div></div>
+        konchum vinnu, These are all my true words!<br/>
+        'I Love You' emukka nenu chalasarlu cheppunta kadha neeku. <br/>
+        Nijaniki nuvvu em antha andhamga undavu. eadho cuiteness facelo unnadi ante konchum undi ante.<br/>
+        Ina nenu ado andham chusi pramichanu anukunte neekanna andhaga unnavallu chalamandi unnaru. <br/>
+        Kani nenu ninnu eanduku love chestunnano nake ardham kavatam ledu. Naku telisinanta varaku neeka love ante intreste ledu.
+        kani nuvvu eakkado connect aiponu abba naku! ala ani nenu na premani express cheyaglanu kani nannu love cheyou ani ninnu
+        ninnu adgalenu, adi neissam. <br/>
+        Neetho exams avvagane matalu adalinukoni chala excitement-tho vacha! appudu eanduko bhayamvesi neeku degirilo kuda unadaleka poya.
+        <br/>
+        Idi antha chadvina neku em ardam avtundho ledo teliyadu naku!,
+        <br/> kani nadho chinna request pleae! 'nuvvu em cheppli anukuna directga natho chpeva, kani vereokaritho chepenchaku abba please' okna?.
+        <br/>
+        Now don't feel bad and any type of worry, i will do not any texting to you. You should fouces on your thought's 
+        And finaly End this letter...
+        <br/> 
+        @sai
+        
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
     </div>
   );
 };
